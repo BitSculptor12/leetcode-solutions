e403e0c63929aa698de7784e5795cb6d5015043a
Amarjeet raj <amarjeet06042005@gmail.com>
2025-07-18 23:15:44 +0530

    Time: 1 ms (99.76%), Space: 46.2 MB (54.04%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int n= arr.length;
+        int index=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+            arr[index++]=arr[i];
+            }
+        }
+        for(int i=index;i<n;i++){
+            arr[index++]=0;
+        }
+    }
+}
