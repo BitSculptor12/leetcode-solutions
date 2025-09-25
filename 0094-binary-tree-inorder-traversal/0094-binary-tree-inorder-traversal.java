8663aea3e336f38bc4de6174de5e9cd32a0ea497
Amarjeet raj <amarjeet06042005@gmail.com>
2025-09-25 21:50:46 +0530

    Time: 0 ms (100%), Space: 41.9 MB (28.02%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -14,15 +14,46 @@
  * }
  */
 class Solution {
-        private  static void Inorder(TreeNode root,List<Integer> ans){
-        if(root==null) return;
-        Inorder(root.left,ans);
-        ans.add(root.val);
-        Inorder(root.right,ans);
-    }
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        Inorder(root,ans);
-        return ans;
+      TreeNode curr =root;
+      List<Integer> arr = new ArrayList<>();
+      while(curr!=null){
+        if(curr.left!=null){
+            TreeNode pred =curr.left;
+            while(pred.right!=null&&pred.right!=curr){ //this is for link break
+            pred =pred.right;
+            
+            }
+            if(pred.right==null){ //connect pred with curr
+                pred.right=curr;
+                curr=curr.left;
+            }
+            if(pred.right==curr){  //unlink pred and curr
+                pred.right=null;
+                arr.add(curr.val);
+                curr = curr.right;
+            }
+        }
+        else{  //curr/left=null ,no pred  pred=predesor
+arr.add(curr.val);
+curr=curr.right;
+        }
+      }  
+      return arr;
     }
 }
+
+//1st method
+// class Solution {
+//         private  static void Inorder(TreeNode root,List<Integer> ans){
+//         if(root==null) return;
+//         Inorder(root.left,ans);
+//         ans.add(root.val);
+//         Inorder(root.right,ans);
+//     }
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         Inorder(root,ans);
+//         return ans;
+//     }
+// }
