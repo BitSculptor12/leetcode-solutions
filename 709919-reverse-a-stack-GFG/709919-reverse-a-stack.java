b56ad62700fb979da23b356c60f058b328ba2b16
Amarjeet raj <amarjeet06042005@gmail.com>
2026-03-02 12:21:11 +0530

    Added solution - LeetHub

---
+++ 709919-reverse-a-stack-GFG/709919-reverse-a-stack.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static void reverseStack(Stack<Integer> st) {
+        // code here
+        if(st.isEmpty()) return;
+        int top = st.pop();
+        reverseStack(st);
+        insert(st,top);
+    }
+    public static void insert(Stack<Integer> st ,int x){
+        if(st.isEmpty()){
+            st.push(x);
+            return;
+        }
+        int top = st.pop();
+        insert(st,x);
+        st.push(top);
+    }
+}
