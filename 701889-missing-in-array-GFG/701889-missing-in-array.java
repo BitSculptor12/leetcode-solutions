1b029835ba28f47abac1cdb524c45ccb894e8667
Amarjeet raj <amarjeet06042005@gmail.com>
2026-03-07 20:28:19 +0530

    Added solution - LeetHub

---
+++ 701889-missing-in-array-GFG/701889-missing-in-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    int missingNum(int arr[]) {
+        // code here
+        long n = arr.length+1;
+        long exp =n*(n+1)/2;
+        int sum =0;
+        for(int i=0;i<arr.length;i++){
+            sum +=arr[i];
+        }
+        return  (int)exp-sum;
+    }
+}
