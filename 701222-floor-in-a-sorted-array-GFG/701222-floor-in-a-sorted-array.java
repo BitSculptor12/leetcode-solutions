5518fcc68bfd324fc823dfe44c39d09fe702ab45
Amarjeet raj <amarjeet06042005@gmail.com>
2026-04-02 01:04:34 +0530

    Added solution - LeetHub

--- 701222-floor-in-a-sorted-array-GFG/701222-floor-in-a-sorted-array.java
+++ 701222-floor-in-a-sorted-array-GFG/701222-floor-in-a-sorted-array.java
@@ -2,18 +2,18 @@ class Solution {
     public int findFloor(int[] arr, int x) {
         // code here
         int n = arr.length;
-        int l=0,h = n-1;
-        int ub = n;
+        int l=0,h=n-1;
+        int lb = -1;
         while(l<=h){
-            int mid = l+(h-l)/2;
-            if(arr[mid]>x){
-                ub = Math.min(ub,mid);
-                h = mid-1;
+            int mid =l+(h-l)/2;
+            if(arr[mid]<=x){
+                lb = mid;
+                l= mid+1;
             }
             else{
-                l = mid+1;
+                h =mid-1;
             }
         }
-        return ub-1;
+        return lb;
     }
 }
