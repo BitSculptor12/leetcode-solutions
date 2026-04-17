a29a3bd36cc629fc16331a4eb2e22017b6e6e925
Amarjeet raj <amarjeet06042005@gmail.com>
2026-04-17 22:48:21 +0530

    Added solution - LeetHub

--- 702691--GFG/702691-.java
+++ 702691--GFG/702691-.java
@@ -3,29 +3,31 @@
 class Solution {
     public ArrayList<Integer> findSubarray(int arr[]) {
         // code here
-        int n =arr.length;
-        ArrayList<Integer> ans = new ArrayList<>();
+        int n = arr.length;
+        
         ArrayList<Integer> curr = new ArrayList<>();
-        int currSum=0;
+        ArrayList<Integer> ans = new ArrayList<>();
+        int currSum =0;
         int bestSum=-1;
         for(int i=0;i<n;i++){
             if(arr[i]>=0){
                 curr.add(arr[i]);
-                currSum += arr[i];
+                currSum +=arr[i];
             }
             else{
-                if(currSum>bestSum || (currSum==bestSum&&curr.size()>ans.size())){
-                    ans=new ArrayList<>(curr);
-                    bestSum = currSum;
+                if(currSum>bestSum||((currSum==bestSum)&&curr.size()>ans.size())){
+                    ans = new ArrayList<>(curr);
+                    bestSum =currSum;
+                    
                 }
-                currSum=0;
                 curr.clear();
+                currSum=0;
             }
         }
-        if(currSum>bestSum ||(currSum==bestSum&&curr.size()>ans.size())){
-            ans =new ArrayList<>(curr);
-        }
-        if(ans.size()==0) ans.add(-1);
-        return ans;
+         if(currSum>bestSum||(currSum==bestSum)&&curr.size()>ans.size()){
+                    ans = new ArrayList<>(curr);
+            }
+            if(ans.size()==0) ans.add(-1);
+    return ans;
     }
 }
