7fdd3f8926a6e398ee831d21a2b3bdd7618fd418
Amarjeet raj <amarjeet06042005@gmail.com>
2026-04-21 23:54:14 +0530

    Added solution - LeetHub

--- 702768-common-in-3-sorted-arrays-GFG/702768-common-in-3-sorted-arrays.java
+++ 702768-common-in-3-sorted-arrays-GFG/702768-common-in-3-sorted-arrays.java
@@ -1,27 +1,21 @@
 class Solution {
     public ArrayList<Integer> commonElements(int[] a, int[] b, int[] c) {
-        int i = 0, j = 0, k = 0;
+        // code here
+        int i=0,j=0,k=0;
         ArrayList<Integer> res = new ArrayList<>();
-        
-        while(i < a.length && j < b.length && k < c.length){
-            
-            if(a[i] == b[j] && b[j] == c[k]){
-                if(res.size() == 0 || res.get(res.size()-1) != a[i]){
+        while(i<a.length&&j<b.length&&k<c.length){
+            if(a[i]==b[j]&&b[j]==c[k]){
+                if(res.size()==0||res.get(res.size()-1)!=a[i]){
                     res.add(a[i]);
                 }
-                i++; j++; k++;
-            }
-            else if(a[i] < b[j]){
                 i++;
-            }
-            else if(b[j] < c[k]){
                 j++;
-            }
-            else{
                 k++;
             }
+            else if(a[i]<b[j]) i++;
+            else if(b[j]<c[k]) j++;
+            else k++;
         }
-        
         return res;
     }
 }
