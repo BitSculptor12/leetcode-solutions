8c39797ce0a9d2300688da19336251f05e0778e7
Amarjeet raj <amarjeet06042005@gmail.com>
2026-04-21 10:06:41 +0530

    Added solution - LeetHub

--- 701226-count-inversions-GFG/701226-count-inversions.java
+++ 701226-count-inversions-GFG/701226-count-inversions.java
@@ -1,54 +1,43 @@
 class Solution {
-
-    static int inversionCount(int arr[]) {
-        return mergeSort(arr, 0, arr.length - 1);
-    }
-
-    static int mergeSort(int[] arr, int low, int high) {
-        int count = 0;
-
-        if (low < high) {
-            int mid = (low + high) / 2;
-
-            count += mergeSort(arr, low, mid);      // left
-            count += mergeSort(arr, mid + 1, high); // right
-
-            count += merge(arr, low, mid, high);    // merge + count
-        }
-
-        return count;
-    }
-
-    static int merge(int[] arr, int low, int mid, int high) {
-        int[] temp = new int[high - low + 1];
-
-        int i = low;
-        int j = mid + 1;
-        int k = 0;
-        int count = 0;
-
-        while (i <= mid && j <= high) {
-            if (arr[i] <= arr[j]) {
+    public static int merge(int arr[],int l,int mid,int h){
+        int i= l;
+        int j = mid+1;
+        int k=0;
+        int count =0;
+        int temp[] = new int[h-l+1];
+        while(i<=mid&&j<=h){
+            if(arr[i]<=arr[j]){
                 temp[k++] = arr[i++];
-            } else {
+            }
+            else{
                 temp[k++] = arr[j++];
-                count += (mid - i + 1); // 🔥 MAIN LOGIC
+                count +=mid-i+1;
             }
         }
-
-        while (i <= mid) {
+        while(i<=mid){
             temp[k++] = arr[i++];
         }
-
-        while (j <= high) {
-            temp[k++] = arr[j++];
+        while(j<=h){
+            temp[k++] =arr[j++];
         }
-
-        // copy back
-        for (int x = 0; x < temp.length; x++) {
-            arr[low + x] = temp[x];
+        for(int x=0;x<temp.length;x++){
+            arr[l+x]=temp[x];
         }
-
         return count;
+    }
+    public static int mergeSort(int arr[],int l,int h){
+        int count =0;
+        if(l<h){
+            int mid = l+(h-l)/2;
+            count +=mergeSort(arr,l,mid);
+            count +=mergeSort(arr,mid+1,h);
+            count +=merge(arr,l,mid,h);
+        }
+        return count;
+    }
+    static int inversionCount(int arr[]) {
+        // Code Here
+        int n = arr.length;
+        return mergeSort(arr,0,n-1);
     }
 }
