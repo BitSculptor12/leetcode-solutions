05439018089a1e6932fa72c0dbd43871211b54f9
Amarjeet raj <amarjeet06042005@gmail.com>
2026-04-22 07:33:02 +0530

    Added solution - LeetHub

--- 702922-alternate-positive-negative-GFG/702922-alternate-positive-negative.java
+++ 702922-alternate-positive-negative-GFG/702922-alternate-positive-negative.java
@@ -1,36 +1,27 @@
 class Solution {
     void rearrange(ArrayList<Integer> arr) {
-        
+        // code here
         ArrayList<Integer> pos = new ArrayList<>();
         ArrayList<Integer> neg = new ArrayList<>();
         
-        for(int val : arr){
-            if(val >= 0) pos.add(val);
+        for(int val:arr){
+            if(val>=0) pos.add(val);
             else neg.add(val);
         }
-        
-        int n = arr.size();
-        int[] brr = new int[n];
-        
-        int p = 0, ne = 0, i = 0;
-        
-        // alternate (start with positive)
-        while(p < pos.size() && ne < neg.size()){
-            brr[i++] = pos.get(p++);
-            brr[i++] = neg.get(ne++);
+        int i=0;int p=0,n=0;
+        int l = arr.size();
+        int brr[] = new int[l];
+        while(p<pos.size()&&n<neg.size()){
+            brr[i++] =pos.get(p++);
+            brr[i++]=neg.get(n++);
         }
-        
-        // remaining positives (AS IT IS)
-        while(p < pos.size()){
-            brr[i++] = pos.get(p++);
+        while(p<pos.size()){
+            brr[i++]=pos.get(p++);
         }
-        
-        // remaining negatives (AS IT IS)
-        while(ne < neg.size()){
-            brr[i++] = neg.get(ne++);
+        while(n<neg.size()){
+            brr[i++]=neg.get(n++);
         }
-        
-        for(int j = 0; j < n; j++){
+         for(int j = 0; j < l; j++){
             arr.set(j, brr[j]);
         }
     }
