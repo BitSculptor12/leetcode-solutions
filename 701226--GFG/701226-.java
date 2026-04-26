bb3ea8821a345ea44d7edacaa69fd7c2b2aacda2
Amarjeet raj <amarjeet06042005@gmail.com>
2026-04-26 05:57:19 +0530

    Added solution - LeetHub

--- 701226--GFG/701226-.java
+++ 701226--GFG/701226-.java
@@ -1,10 +1,10 @@
 class Solution {
-    public static  int merge(int arr[],int l,int mid,int h){
-        int i=l;
-        int j = mid+1;
+    static int mergeSort(int arr[],int l,int mid, int h){
         int temp[] = new int[h-l+1];
-        int k =0;
         int count =0;
+        int i=l;
+        int j=mid+1;
+        int k=0;
         while(i<=mid&&j<=h){
             if(arr[i]<=arr[j]){
                 temp[k++]=arr[i++];
@@ -21,23 +21,24 @@ class Solution {
             temp[k++]=arr[j++];
         }
         for(int x=0;x<temp.length;x++){
-            arr[l+x]=temp[x];
+            arr[x+l]=temp[x];
         }
         return count;
+        
     }
-    public static int mergeSort(int arr[],int l,int h){
+    static int merge(int arr[],int l,int h){
         int count =0;
         if(l<h){
             int mid =l+(h-l)/2;
-            count +=mergeSort(arr,l,mid);
-            count += mergeSort(arr,mid+1,h);
-            count +=merge(arr,l,mid,h);
+           count +=merge(arr,l,mid);
+           count +=merge(arr,mid+1,h);
+           count +=mergeSort(arr,l,mid,h);
         }
         return count;
     }
     static int inversionCount(int arr[]) {
         // Code Here
         int n = arr.length;
-        return mergeSort(arr,0,n-1);
+        return merge(arr,0,n-1);
     }
 }
