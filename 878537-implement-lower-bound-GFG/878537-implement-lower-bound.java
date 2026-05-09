c5805f3fd71958f7ad9951e38a2bdaa9e00783cb
Amarjeet raj <amarjeet06042005@gmail.com>
2026-05-09 20:42:52 +0530

    Added solution - LeetHub

--- 878537-implement-lower-bound-GFG/878537-implement-lower-bound.java
+++ 878537-implement-lower-bound-GFG/878537-implement-lower-bound.java
@@ -2,13 +2,14 @@ class Solution {
     int lowerBound(int[] arr, int target) {
         // code here
         int n = arr.length;
-        int l=0, h= n-1;
+        int l=0;
+        int h = n-1;
         int lb = n;
         while(l<=h){
             int mid = l+(h-l)/2;
             if(arr[mid]>=target){
                 lb = Math.min(lb,mid);
-                h = mid-1;
+                h= mid-1;
             }
             else{
                 l = mid+1;
