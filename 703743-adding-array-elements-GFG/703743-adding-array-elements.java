9b8d7e269cb36c6600e69df01d27ae45dd1f16b2
Amarjeet raj <amarjeet06042005@gmail.com>
2026-05-18 10:28:19 +0530

    Added solution - LeetHub

---
+++ 703743-adding-array-elements-GFG/703743-adding-array-elements.java
@@ -0,0 +1,30 @@
+class Solution {
+    int minOperations(int[] arr, int n, int k) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int ele : arr) {
+            pq.add(ele);
+        }
+
+        int count = 0;
+
+        while(pq.size() > 1 && pq.peek() < k) {
+
+            int v1 = pq.poll();
+            int v2 = pq.poll();
+
+            pq.add(v1 + v2);
+
+            count++;
+        }
+
+        if(pq.peek() >= k) return count;
+
+        return -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
