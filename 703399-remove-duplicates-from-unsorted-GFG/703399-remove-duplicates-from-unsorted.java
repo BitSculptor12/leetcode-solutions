4384af836b6e45cf12999f2f1bd073ffa51ee59e
Amarjeet raj <amarjeet06042005@gmail.com>
2026-05-18 08:53:04 +0530

    Added solution - LeetHub

---
+++ 703399-remove-duplicates-from-unsorted-GFG/703399-remove-duplicates-from-unsorted.java
@@ -0,0 +1,16 @@
+class Solution {
+    static ArrayList<Integer> removeDuplicate(int arr[]) {
+
+        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+
+        for(int num : arr) {
+            set.add(num);
+        }
+
+        return new ArrayList<>(set);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
